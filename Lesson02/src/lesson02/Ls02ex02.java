9dd895a578399090153616062a6e3f74c4b1a0d1
AlexStibbons <43070613+AlexStibbons@users.noreply.github.com>
2018-09-14 17:15:31 +0200

    additions to ex3 and 2

--- Lesson02/src/lesson02/Ls02ex02.java
+++ Lesson02/src/lesson02/Ls02ex02.java
@@ -3,7 +3,7 @@ package lesson02;
 public class Ls02ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//use the % !!! 	
 
 	}
 
--- Lesson02/src/lesson02/Ls02ex03.java
+++ Lesson02/src/lesson02/Ls02ex03.java
@@ -3,7 +3,36 @@ package lesson02;
 public class Ls02ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	double x = 35;
+	double y = 15;
+
+	if (x > y) {
+		double min = y;
+		double max = x;
+	} else {
+		double min = x;
+		double max = y;
+	}
+
+	Sting operacija = "switch";
+
+	switch (operacija) {
+		Case "min":
+		System.out.println(min);
+		break;
+		Case "max":
+		System.out.println(max);
+		break;
+		Case "swap":
+		//something
+		break;
+		Case "equals":
+		//something
+		break;
+		default:
+		System.out.println("Incorrect input");
+	}
 
 	}
 
